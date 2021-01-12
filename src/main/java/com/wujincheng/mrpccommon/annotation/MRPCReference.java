94a8de60d2ce062e556539b7b195cfa3585306bb
wujincheng2333 <738126916@qq.com>
2021-01-12 14:28:59 +0800

    注解

---
+++ src/main/java/com/wujincheng/mrpccommon/annotation/MRPCReference.java
@@ -0,0 +1,15 @@
+package com.wujincheng.mrpccommon.annotation;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+
+@Target({FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MRPCReference {
+
+
+}
